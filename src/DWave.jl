8aed07738a24bc4b53e2eadb515435cae3fb10f7
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-10-10 16:04:00 -0300

    Finish package setup

--- src/DWave.jl
+++ src/DWave.jl
@@ -4,10 +4,14 @@ using Anneal
 using PythonCall
 
 # -*- :: Python D-Wave Module :: -*- #
+const dwave_dimod = PythonCall.pynew()
 const dwave_cloud = PythonCall.pynew()
+const dwave_embedding = PythonCall.pynew()
 
 function __init__()
+    PythonCall.pycopy!(dwave_dimod, pyimport("dimod"))
     PythonCall.pycopy!(dwave_cloud, pyimport("dwave.cloud"))
+    PythonCall.pycopy!(dwave_embedding, pyimport("dwave.embedding"))
 end
 
 Anneal.@anew Optimizer begin
@@ -22,21 +26,28 @@ end
 
 function Anneal.sample(sampler::Optimizer{T}) where {T}
     # Ising Model
-    J, h = Anneal.ising(Dict, T, sampler)
+    h, J, α, β = Anneal.ising(Dict, T, sampler)
+
+    # D-Wave's BQM
+    bqm = DWave.dwave_dimod.BinaryQuadraticModel.from_ising(h, J)
 
     # Attributes
     num_reads = MOI.get(sampler, MOI.RawOptimizerAttribute("num_reads"))
 
+    # -*- Timing Information -*- #
+    time_data = Dict{String,Any}()
+
     # Results vector
     samples = Anneal.Sample{Int,T}[]
 
-    # Time data
-    time_data = Dict{String,Any}()
-
     connect() do client
         solver = client.get_solver()
-        result = @timed solver.sample_ising(J, h; num_reads=num_reads)
-        record = result.value.record
+        
+        χ, hχ, Jχ = embed(solver, h, J)
+
+        result = @timed solver.sample_ising(hχ, Jχ; num_reads=num_reads)
+        future = result.value
+        record = unembed(future.sampleset, χ, bqm).record
 
         for (ψ, e, k) in record
             sample = Anneal.Sample{Int,T}(
@@ -74,6 +85,29 @@ function connect(callback)
     finally
         client.close()
     end
+end
+
+function embed(solver, h::Dict{Int,T}, J::Dict{Tuple{Int,Int},T}) where {T}
+    χ = DWave.dwave_embedding.minorminer.find_embedding(
+        keys(J),
+        solver.edges
+    )
+
+    A = Dict{Int, Vector{Int}}(v => Int[] for v in pyconvert.(Int, solver.nodes))
+
+    for e in solver.edges
+        u, v = pyconvert.(Int, e)
+        push!(A[u], v)
+        push!(A[v], u)
+    end
+
+    hχ, Jχ = DWave.dwave_embedding.embed_ising(h, J, χ, A)
+
+    return (χ, hχ, Jχ)
+end
+
+function unembed(sampleset, χ, bqm)
+    return DWave.dwave_embedding.unembed_sampleset(sampleset, χ, bqm)
 end
 
 end # module
