1588bc39dfd11bae2571b9391a0324b7d23a77a5
pedromxavier <pedromxavier@poli.ufrj.br>
2023-07-24 20:09:53 -0300

    Adopt Lazy API Token retrieval

--- src/DWave.jl
+++ src/DWave.jl
@@ -13,24 +13,22 @@ import QUBODrivers:
     ising
 
 # -*- :: Python D-Wave Module :: -*- #
-const dwave_dimod = PythonCall.pynew()
 const dwave_cloud = PythonCall.pynew()
-const dwave_system = PythonCall.pynew()
-const dwave_networkx = PythonCall.pynew()
+const dwave_dimod = PythonCall.pynew()
 const dwave_embedding = PythonCall.pynew()
+const dwave_networkx = PythonCall.pynew()
+const dwave_system = PythonCall.pynew()
 
 function __init__()
     # Python Packages
-    PythonCall.pycopy!(dwave_dimod, pyimport("dimod"))
     PythonCall.pycopy!(dwave_cloud, pyimport("dwave.cloud"))
-    PythonCall.pycopy!(dwave_system, pyimport("dwave.system"))
-    PythonCall.pycopy!(dwave_networkx, pyimport("dwave_networkx"))
+    PythonCall.pycopy!(dwave_dimod, pyimport("dimod"))
     PythonCall.pycopy!(dwave_embedding, pyimport("dwave.embedding"))
+    PythonCall.pycopy!(dwave_networkx, pyimport("dwave_networkx"))
+    PythonCall.pycopy!(dwave_system, pyimport("dwave.system"))
 
     # D-Wave API Credentials
-    DWAVE_API_TOKEN = get(ENV, "DWAVE_API_TOKEN", nothing)
-
-    if isnothing(DWAVE_API_TOKEN)
+    if !haskey(ENV, "DWAVE_API_TOKEN")
         @warn """
         The 'DWAVE_API_TOKEN' environment variable is not defined. Please, make sure that another access method is available.
         
@@ -61,7 +59,9 @@ function sample(sampler::Optimizer{T}) where {T}
 
     if dwave_sampler === nothing
         dwave_sampler = dwave_system.EmbeddingComposite(
-            dwave_system.DWaveSampler()
+            dwave_system.DWaveSampler(;
+                token = get(ENV, "DWAVE_API_TOKEN", nothing)
+            )
         )
     end
 
@@ -76,14 +76,14 @@ function sample(sampler::Optimizer{T}) where {T}
 
     result = @timed dwave_sampler.sample_ising(h, J; num_reads=num_reads)
 
-    for (ψ, e, k) in result.value.record
+    for (ψ, λ, r) in result.value.record
         sample = Sample{T}(
             # state:
             pyconvert.(Int, ψ),
             # value: 
-            α * (pyconvert(T, e) + β),
+            α * (pyconvert(T, λ) + β),
             # reads:
-            pyconvert(Int, k),
+            pyconvert(Int, r),
         )
 
         push!(samples, sample)
