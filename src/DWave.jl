96c41abdb8f8ea8e4a7fbdca83fffe93dad8a108
pedromxavier <pedromxavier@poli.ufrj.br>
2023-07-24 20:41:15 -0300

    Include `numpy` + `matplotlib`

--- src/DWave.jl
+++ src/DWave.jl
@@ -13,14 +13,18 @@ import QUBODrivers:
     ising
 
 # -*- :: Python D-Wave Module :: -*- #
-const dwave_cloud = PythonCall.pynew()
-const dwave_dimod = PythonCall.pynew()
+const np  = PythonCall.pynew()
+const plt = PythonCall.pynew()
+const dwave_cloud     = PythonCall.pynew()
+const dwave_dimod     = PythonCall.pynew()
 const dwave_embedding = PythonCall.pynew()
-const dwave_networkx = PythonCall.pynew()
-const dwave_system = PythonCall.pynew()
+const dwave_networkx  = PythonCall.pynew()
+const dwave_system    = PythonCall.pynew()
 
 function __init__()
     # Python Packages
+    PythonCall.pycopy!(np, pyimport("numpy"))
+    PythonCall.pycopy!(plt, pyimport("matplotlib.pyplot"))
     PythonCall.pycopy!(dwave_cloud, pyimport("dwave.cloud"))
     PythonCall.pycopy!(dwave_dimod, pyimport("dimod"))
     PythonCall.pycopy!(dwave_embedding, pyimport("dwave.embedding"))
