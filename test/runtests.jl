8a85310b94df7926e033fa7605cc8fd87949a16c
Pedro Ripper <pedros.ripper@globo.com>
2024-05-28 23:06:18 -0300

    Fix DWave error and return tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using DWave: DWave, QUBO.QUBODrivers
 
-# QUBODrivers.test(DWave.Optimizer; examples = true)
+QUBODrivers.test(DWave.Optimizer; examples = true)
 QUBODrivers.test(DWave.Neal.Optimizer; examples = true)
